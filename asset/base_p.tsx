74f45f7d25605ca91b87fc7adcbf917bbca6d058
blinkingnoise <tom.hlina@blinkingnoise.org>
2019-11-23 01:43:15 +0100

    initial commit

---
+++ asset/base_p.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="base" tilewidth="8" tileheight="8" tilecount="6232">
+ <image source="Game Boy GBC - Pokemon Red Blue - Tileset.png" width="608" height="656"/>
+</tileset>
